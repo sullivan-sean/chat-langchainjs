903fb92bf0f47fa224ee83d631f7b09aaf79e914
Sean Sullivan <ssullivan61198@gmail.com>
2023-02-18 21:35:33 -0800

    fix: includeFiles

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,12 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import path from 'path';
+import fs from 'fs';
 import { HNSWLib } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from 'langchain/embeddings';
 import { makeChain } from "./util";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body;
-  const vectorstore = await HNSWLib.load("data", new OpenAIEmbeddings())
+  const dir = path.resolve(process.cwd(), 'data');
+  try {
+    console.log(fs.readdirSync(process.cwd()))
+  } catch (e) {
+    console.log("Failed to read cwd");
+  }
+
+  try {
+    console.log(fs.readdirSync(dir))
+  } catch (e) {
+    console.log("Failed to read data dir");
+  }
+  const vectorstore = await HNSWLib.load(dir, new OpenAIEmbeddings())
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     // Important to set no-transform to avoid compression, which will delay
