bce78a80636411c363b595b55a56dbc2cc60ae28
amnano <63117827+amnano@users.noreply.github.com>
2023-03-02 11:09:13 +0100

    typo

--- pages/api/util.ts
+++ pages/api/util.ts
@@ -11,7 +11,12 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant for a consulting firm.
+  `You are an AI assistant for the open source library LangChain. The documentation is located at https://langchain.readthedocs.io.
+You are given the following extracted parts of a long document and a question. Provide a conversational answer with a hyperlink to the documentation.
+You should only use hyperlinks that are explicitly listed as a source in the context. Do NOT make up a hyperlink that is not listed.
+If the question includes a request for code, provide a code block directly from the documentation.
+If you don't know the answer, just say "Hmm, I'm not sure." Don't try to make up an answer.
+If the question is not about LangChain, politely inform them that you are tuned to only answer questions about LangChain.
 Question: {question}
 =========
 {context}
